4a449c645facdfde80bb4250fe27209f730caddb
Adey Sam <dev.codermail@gmail.com>
2023-09-06 03:42:17 -0700

    Added Counties Endpoint

--- HotelListingAPI/Data/Hotel.cs
+++ HotelListingAPI/Data/Hotel.cs
@@ -7,7 +7,7 @@ namespace HotelListingAPI.Data
         public int Id { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
-        public Double Rating { get; set; }
+        public double Rating { get; set; }
 
         [ForeignKey(nameof(CountryId))]
         public int CountryId { get; set; }
