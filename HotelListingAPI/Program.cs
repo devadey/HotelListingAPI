b7c9634e4b8649a784c57077873e8be8d2257f8b
Adey Sam <dev.codermail@gmail.com>
2023-09-05 18:53:14 -0700

    Add project files.

---
+++ HotelListingAPI/Program.cs
@@ -0,0 +1,50 @@
+using HotelListingAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+
+var connectionString = builder.Configuration.GetConnectionString("HotelListinDbConnectionString");
+builder.Services.AddDbContext<HotelListingDbContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Add Cors Configuration
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", b=>b.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
+});
+
+//use serilog
+builder.Host.UseSerilog((context, logger) => logger.WriteTo.Console().ReadFrom.Configuration(context.Configuration));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseSerilogRequestLogging();
+
+app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
